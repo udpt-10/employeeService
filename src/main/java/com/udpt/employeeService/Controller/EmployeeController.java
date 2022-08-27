072f3dbc806a42622832e24ce2e067cb6e77e033
BerNardTran <tbkhanh0908@gmail.com>
2022-08-27 15:18:17 +0700

    Update EmployeeController.java

--- src/main/java/com/udpt/employeeService/Controller/EmployeeController.java
+++ src/main/java/com/udpt/employeeService/Controller/EmployeeController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Controller
 @RestController
 @RequestMapping("/employee")
-@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
 public class EmployeeController {
 
     @Autowired
